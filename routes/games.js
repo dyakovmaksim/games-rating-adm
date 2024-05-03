fe9aadd362353920f15924fcadfae0da77270c55
dyakovmaksim <pochta3478@mail.ru>
2024-05-03 18:07:42 +0500

    Дэзэшечка на проверку

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,9 @@
 const gamesRouter = require("express").Router();
 const { sendAllGames, deleteGame, addGameController } = require("../controllers/games");
-const { getAllGames } = require("../middlewares/games");
+const { getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, findGameById, deleteGame } = require("../middlewares/games");
 
-gamesRouter.post("/games", getAllGames, addGameController);
+gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, addGameController);
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.delete("/games/:id", getAllGames, deleteGame, findGameById);
 
 module.exports = gamesRouter;
